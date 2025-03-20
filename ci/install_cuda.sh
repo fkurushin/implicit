e02c5874897b33d14faf7f4bec6563d4e616313c
Fedor Kurushin <75683893+fkurushin@users.noreply.github.com>
2025-03-20 11:39:06 +0300

    Update install_cuda.sh

--- ci/install_cuda.sh
+++ ci/install_cuda.sh
@@ -1,9 +1,9 @@
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum install --setopt=obsoletes=0 -y \
-    cuda-nvcc-11-4 \
-    cuda-cudart-devel-11-4 \
-    libcurand-devel-11-4 \
-    libcublas-devel-11-4 \
-    cuda-nvprof-11-4 \
+    cuda-nvcc-12-6 \
+    cuda-cudart-devel-12-6 \
+    libcurand-devel-12-6 \
+    libcublas-devel-12-6 \
+    cuda-nvprof-12-6 \
     ninja-build
-ln -s cuda-11.4 /usr/local/cuda
+ln -s cuda-12.6 /usr/local/cuda
